3ab6c22fe78d97637984c4df9a60dcda97e57dcf
David Chevallier <david@chevallier.in>
2024-05-21 10:19:12 +0200

    first translation to de

--- metadata/timeIdentifiers.js
+++ metadata/timeIdentifiers.js
@@ -4,48 +4,55 @@ module.exports = {
         time: "מיידי",
         time_en: "Immediately",
         time_ru: "Немедленно",
-        time_ar: "في الحال"
+        time_ar: "في الحال",
+        time_de: "Sofort"
     },
     "15 שניות": {
         countdown: 15,
         time: "15 שניות",
         time_en: "15 seconds",
         time_ru: "15 секунд",
-        time_ar: "۱٥ ثانية"
+        time_ar: "۱٥ ثانية",
+        time_de: "15 Sekunden"
     },
     "30 שניות": {
         countdown: 30,
         time: "30 שניות",
         time_en: "30 seconds",
         time_ru: "30 секунд",
-        time_ar: "۳۰ ثانية"
+        time_ar: "۳۰ ثانية",
+        time_de: "30 Sekunden"
     },
     "45 שניות": {
         countdown: 45,
         time: "45 שניות",
         time_en: "45 seconds",
         time_ru: "45 секунд",
-        time_ar: "٤٥ ثانية"
+        time_ar: "٤٥ ثانية",
+        time_de: "45 Sekunden"
     },
     "דקה": {
         countdown: 60,
         time: "דקה",
         time_en: "A minute",
         time_ru: "Минута",
-        time_ar: "دقيقة"
+        time_ar: "دقيقة",
+        time_de: "Eine Minute"
     },
     "דקה וחצי": {
         countdown: 90,
         time: "דקה וחצי",
         time_en: "A minute and a half",
         time_ru: "Полторы минуты",
-        time_ar: "دقيقة ونصف"
+        time_ar: "دقيقة ونصف",
+        time_de: "Eineinhalb Minuten"
     },
     "3 דקות": {
         countdown: 180,
         time: "3 דקות",
         time_en: "3 minutes",
         time_ru: "3 минуты",
-        time_ar: "۳ دقائق"
+        time_ar: "۳ دقائق",
+        time_de: "3 Minuten"
     }
 };
