b8ffabef2ea481f0be7a2a283df3699e43ffb60f
David Chevallier <david@chevallier.in>
2024-05-21 11:21:50 +0200

    anpassungen zur code Darstellung

--- scripts/docu.sh
+++ scripts/docu.sh
@@ -1,29 +1,33 @@
 #!/usr/bin/env bash
 #shellcheck shell=bash
-#!/bin/bash
-
 
 doc_dir="docs"
 mkdir -p $doc_dir
 output_file="$doc_dir/documentation.md"
 
-echo "# Projekt Dokumentation" > $output_file
-echo "Hier sind die extrahierten Dateien aus dem Code:" >> $output_file
+echo "# Project Documentation" > $output_file
+echo "Below are the extracted files from the code:" >> $output_file
 echo "" >> $output_file
 
-find . -type f \( -name "*.js" -o -name "*.json" \) ! -name "polygons.json" ! -name "cities.json" ! -path "./node_modules/*" | while read file; do
-    echo "## Datei: $file" >> $output_file
-    
-    if [[ $file == *.js ]]; then
-        language="javascript"
-    elif [[ $file == *.json ]]; then
-        language="json"
-    fi
+find . -type f \( -name "*.js" -o -name "*.json" \) ! -name "polygons.json" ! -path "./node_modules/*" | while read file; do
+    echo "## File: $file" >> $output_file
 
-    echo '```'"$language" >> $output_file
-    cat "$file" >> $output_file
-    echo '' >> $output_file
-    echo '```' >> $output_file
+    processing=0
+    highlight=""
+
+    while IFS= read -r line; do
+        if [[ "$line" =~ @doc-start:(.*) ]]; then
+            highlight="${BASH_REMATCH[1]}"
+            echo "```$highlight" >> $output_file
+            processing=1
+        elif [[ "$line" =~ @doc-end ]]; then
+            # End processing and close the code block
+            echo "```" >> $output_file
+            processing=0
+        elif [[ "$processing" -eq 1 ]]; then
+            echo "$line" >> $output_file
+        fi
+    done < "$file"
     echo "" >> $output_file
 done
 
